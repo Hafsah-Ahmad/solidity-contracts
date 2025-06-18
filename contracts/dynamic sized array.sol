abad514b9176fb42cf80ad52df65bdc6581181b4
Hafsa Ahmad <hafsa.ahmad043@gmail.com>
2025-06-18 22:17:29 +0500

    contains different basic smart contracts

---
+++ contracts/dynamic sized array.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0<0.9.0;
+contract myContract{
+    string[]public student;
+    function addVal()public{
+        student.push("hafsa");
+        student.push("fatima");
+        student.push("jaisha");
+        student.push("ayesha");
+        student.push("shazi");
+
+}
+      function del()public{
+        student.pop();
+      }
+}
